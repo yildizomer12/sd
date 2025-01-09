043b6fec875fc82faa33753c8c1997bbf561de05
Mtr+-60891011 <omer1476hotmail@gmail.com>
2025-01-10 02:22:00 +0300

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Footer from "./components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +16,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
