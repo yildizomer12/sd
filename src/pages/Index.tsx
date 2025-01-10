2409374dbacbecb272e79c99026280e46e3aa7f2
Mtr+-60891011 <omer1476hotmail@gmail.com>
2025-01-10 09:30:20 +0300

    Add Footer component to Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import { Hero } from "@/components/Hero";
 import { ProductionLine } from "@/components/ProductionLine";
 import { TechnologyAdvantages } from "@/components/TechnologyAdvantages";
 import { ContactForm } from "@/components/ContactForm";
+import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
@@ -12,6 +13,7 @@ const Index = () => {
       <ProductionLine />
       <TechnologyAdvantages />
       <ContactForm />
+      <Footer />
     </div>
   );
 };
